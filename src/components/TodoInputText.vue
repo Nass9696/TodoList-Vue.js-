6748899f030c871c39bde9846350c0be813595c3
Nassim Gacem <dev.nassim.pro@gmail.com>
2019-10-07 20:26:30 +0200

    Insert an input in TodoInputText.vue

--- src/components/TodoInputText.vue
+++ src/components/TodoInputText.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h2>Julien afa dofus</h2>
+  <div class="control has-icons-left has-icons-right">
+    <input
+      class="input is-medium"
+      type="text"
+      placeholder="Enter your task"
+      :value="value"
+      v-on="listeners"
+    />
+    <span class="icon is-left">
+      <i class="fas fa-check"></i>
+    </span>
   </div>
 </template>
 
